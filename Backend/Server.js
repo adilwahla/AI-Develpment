cfeb5ef42429c91a7280fa35faad2c775afa67bc
Adil Rehman <adilwahla360@gmail.com>
2023-11-15 17:48:51 +0500

    added footer

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
 const cors = require('cors');
-
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 // Enable CORS for all routes
 app.use(cors());
-
+const dbURI = process.env.MONGO_URI;
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -18,11 +18,11 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(authRouter);
 
-const DB =
-  "mongodb+srv://AIGeneration:AIGeneration@cluster0.xi1yz2v.mongodb.net/AIDevelopment?retryWrites=true&w=majority";
+// const DB =
+//   "mongodb+srv://AIGeneration:AIGeneration@cluster0.xi1yz2v.mongodb.net/AIDevelopment?retryWrites=true&w=majority";
 
 mongoose
-  .connect(DB)
+  .connect(dbURI)
   .then(() => {
     console.log("Connection Successful");
   })
