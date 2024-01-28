0b892312699f7a46cb9cc3946af474b642044f46
Adil Rehman <adilwahla360@gmail.com>
2024-01-28 22:33:15 +0500

    added db url

--- Front End/lib/config_dev.dart
+++ Front End/lib/config_dev.dart
@@ -1,4 +1,5 @@
 // config_dev.dart
 class AppConfig {
-  static const String baseUrl = 'http://localhost:3000';
+  // static const String baseUrl = 'http://localhost:3000';
+  static const String baseUrl = "https://operify.onrender.com";
 }
