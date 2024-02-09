7479d9bcd5c002d58ca417a77a456dcdb3b25659
Adil Rehman <adilwahla360@gmail.com>
2024-02-09 23:22:00 +0500

    updated

--- Front End/lib/config_dev.dart
+++ Front End/lib/config_dev.dart
@@ -1,6 +1,6 @@
 // config_dev.dart
 class AppConfig {
-// static const String baseUrl = 'http://localhost:3000';
-  static const String baseUrl =
-      "http://ec2-51-20-250-161.eu-north-1.compute.amazonaws.com:3000";
+ static const String baseUrl = 'http://localhost:3000';
+ // static const String baseUrl =
+   //   "http://ec2-51-20-250-161.eu-north-1.compute.amazonaws.com:3000";
 }
